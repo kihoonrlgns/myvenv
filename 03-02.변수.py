a03788f92d93dacd4411c982816e45fff0df1abf
Kihoon Lee <36075511+kihoonrlgns@users.noreply.github.com>
2021-07-15 00:50:46 +0900

    Add files via upload

---
+++ 03-02.변수.py
@@ -0,0 +1,10 @@
+name = "마스터이"
+level = 5
+health = 800
+attack = 90
+print(name, level, health, attack)
+
+level = level + 1
+health = health + 50
+attack = attack + 10
+print(name, level, health, attack)
